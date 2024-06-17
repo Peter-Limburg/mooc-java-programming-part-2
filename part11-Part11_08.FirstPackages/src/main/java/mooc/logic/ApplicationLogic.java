ff3971eaeccfe1a4119847983df02433b7caf9fa
Pipswolick <129231266+Pipswolick@users.noreply.github.com>
2024-06-17 16:53:56 -0400

    Add files via upload

---
+++ part11-Part11_08.FirstPackages/src/main/java/mooc/logic/ApplicationLogic.java
@@ -0,0 +1,18 @@
+package mooc.logic;
+import mooc.ui.UserInterface;
+public class ApplicationLogic {
+private UserInterface ui;
+    public ApplicationLogic(UserInterface ui){
+        this.ui = ui;
+    }
+
+    public void execute(int times) {
+        int i = 0;
+        while (i < times) {
+            System.out.println("Application logic is working");
+            i++;
+            ui.update();
+        }
+    }
+
+}
